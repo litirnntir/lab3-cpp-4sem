c65c45a26ba96e5a673d4d272b9c6a241b7926d3
salnyed <agorbuncova2004@gmail.com>
2023-05-26 20:44:45 +0400

    Add struct Edge

--- Graph.h
+++ Graph.h
@@ -1,13 +1,37 @@
 #ifndef LAB3_CPP_4SEM__GRAPH_H_
 #define LAB3_CPP_4SEM__GRAPH_H_
 
-#include <iostream>;
+#include <iostream>
+#include <map>
+
 
 template<typename Vertex, typename Distance = double>
-class Graph{
+class Graph
+{
  public:
-	struct Edge{/*...*/};
+	template<typename tVertex, typename tDistance = double>
 
+	// Структура ребра
+	struct Edge
+	{
+		tVertex from;
+		tVertex to;
+		tDistance distance;
+		Edge(tVertex id1 = 0, tVertex id2 = 0, tDistance distance = 0) : from(id1), to(id2), distance(distance) {}
+		friend std::ostream& operator<< (std::ostream& out, const std::map<int, Edge<tVertex>>& map)
+		{
+			for (auto it = map.begin(); it != map.end(); ++it)
+			{
+				out << it->second;
+			}
+			return out;
+		}
+		friend std::ostream& operator<< (std::ostream& out, const Edge<tVertex>& edge)
+		{
+			out << "->(" << edge.from << ", " << edge.to << "; " << edge.distance << ")";
+			return out;
+		}
+	};
 
 	//проверка-добавление-удаление вершин
 	bool has_vertex(const Vertex& v) const;
@@ -15,14 +39,13 @@ class Graph{
 	bool remove_vertex(const Vertex& v);
 	std::vector<Vertex> vertices() const;
 
-
 	//проверка-добавление-удаление ребер
 	void add_edge(const Vertex& from, const Vertex& to,
 		const Distance& d);
 	bool remove_edge(const Vertex& from, const Vertex& to);
 	bool remove_edge(const Edge& e); //c учетом расстояния
 	bool has_edge(const Vertex& from, const Vertex& to) const;
-	bool has_edge (const Edge& e); //c учетом расстояния в Edge
+	bool has_edge(const Edge& e); //c учетом расстояния в Edge
 
 	//получение всех ребер, выходящих из вершины
 	std::vector<Edge> edges(const Vertex& vertex);
@@ -35,8 +58,7 @@ class Graph{
 	std::vector<Edge> shortest_path(const Vertex& from,
 		const Vertex& to) const;
 	//обход
-	std::vector<Vertex>  walk(const Vertex& start_vertex)const;
+	std::vector<Vertex> walk(const Vertex& start_vertex) const;
 };
-
 
 #endif //LAB3_CPP_4SEM__GRAPH_H_
